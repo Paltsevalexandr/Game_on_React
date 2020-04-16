2e7ccb8b15e9d238e18aa5558a00d4ccdd874bdb
Aleksandr <paltsevaleksandr@gmail.com>
2020-04-16 15:31:59 +0300

    failed commit

--- my-app/src/components/checkingShips.js
+++ my-app/src/components/checkingShips.js
@@ -1,7 +1,14 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 
-export function CheckingShips(props) {
+export class CheckingShips extends React.Component{
+  constructor(props) {
+    super(props);
+    this.state = {checkingShips: ['fourdeck1',
+    'threedeck1', 'threedeck2', 'twodeck1',
+    'twodeck2', 'twodeck3', 'onedeck1',
+    'onedeck2', 'onedeck3', 'onedeck4'],};
+  }
   /*let shipBorderColor = '';
   function setShipBorder(e) {
     if(props.currentShip === props.shipName) {
@@ -17,18 +24,20 @@ export function CheckingShips(props) {
   function startDrag(e) {
     e.dataTransfer.setDragImage(i, 10, 10);
   }*/
-  let ships = props.checkingShips.map((item, index)=> {
-    return(
-      <div 
-        className = {"ship " + item}
-        draggable = 'true' key = {index}
+  render() {
+    let ships = props.checkingShips.map((item, index)=> {
+      return(
+        <div 
+          className = {"ship " + item}
+          draggable = 'true' key = {index}
 
-        onMouseDown = {e => props.createCurrentShip(e, item)}
-        onDrag      = {e => props.foundForbiddenCells(e)}>
-      </div>
+          onMouseDown = {e => props.createCurrentShip(e, item)}
+          onDrag      = {e => props.foundForbiddenCells(e)}>
+        </div>
+      );
+    });
+    return(
+      <>{ships}</>
     );
-  });
-  return(
-    <>{ships}</>
-  )
+  }
 }
--- my-app/src/components/positionFunctions/calcShipPosition.js
+++
@@ -1,5 +0,0 @@
-export function calcShipPosition(shipPageXY, shipOffsetXY) {
-  let shipCoordinates = shipPageXY - shipOffsetXY;
-  shipCoordinates = correctPosition(shipCoordinates) + 'px';
-  return shipCoordinates;
-}
